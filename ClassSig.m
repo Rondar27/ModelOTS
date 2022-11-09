c32ccf8762e1514776906300bfae3017b6bd5722
Rondar <rondar.dragon@gmail.com>
2022-11-09 11:47:10 +0300

    saving signal

--- ClassSig.m
+++ ClassSig.m
@@ -13,6 +13,8 @@ classdef ClassSig < handle
         sps; %Выборки на символ
         k;
         sps_k;
+        temp;
+        temp2;
     end
     methods
         function obj = ClassSig(Params, LogLanguage) % Конструктор
@@ -29,7 +31,10 @@ classdef ClassSig < handle
                 obj.LogLanguage = LogLanguage;
             % Переменная которая формирует импульс
                 obj.b = rcosdesign(Sig.beta, Sig.span, Sig.sps);
+                obj.temp=1;
+                obj.temp2=1;
                
+
         end
         function OutData = StepTx(obj, InData)
             if obj.isTransparent
@@ -40,19 +45,25 @@ classdef ClassSig < handle
             
             % Здесь должна быть процедура формирования сигнала
             %ДПВБЧЙФШ  k*sps
+            filename="DataSetSymbols\symbols"+obj.temp+".mat";
+            save(filename,'InData')
+            obj.temp=obj.temp+1;
             OutData = upfirdn(InData, obj.b, obj.sps_k);
+%             plot(real(InData))
         end
         function OutData = StepRx(obj, InData)
             if obj.isTransparent
                 OutData = InData;
                 return
             end
-            
+            filename="DataSetSignals\signals"+obj.temp2+".mat";
+            save(filename,'InData')
+            obj.temp2=obj.temp2+1;
             % Здесь должна быть процедура, обратная поцедуре формирования
             % сигнала
+            
             CF = conv(InData, obj.b); %согласованная фильтрация
-            %ДПВБЧЙФШ  k*sps
-            OutData = CF(obj.span*obj.sps + 1:obj.sps_k:end-obj.span*obj.sps_k);
+            OutData = CF(obj.span*obj.sps + 1:obj.sps_k:end-obj.span*obj.sps);
         end
     end
 end
--- Setup.m
+++ Setup.m
@@ -1,4 +1,5 @@
 % End of Params
+% Common.NumWorkers=4;
 Mapper.isTransparent = false;
 Channel.isTransparent = false;
 Sig.isTransparent = false;
@@ -18,22 +19,25 @@ BER.h2Precision = 0;
 BER.h2dBInitStep = 1;
 BER.h2dBMaxStep = 1;
 BER.h2dBMinStep = 1;
-% Отношения сигнал/шум:
-BER.h2dBInit = 0;
+
+%Отношения сигнал/шум:
+BER.h2dBInit = 10;
 BER.h2dBMax = 10;
 
-% End of Params
+% 1End of Params
+% Common.NumWorkers=4;
 % Mapper.isTransparent = false;
 % Channel.isTransparent = false;
-% Sig.isTransparent = true;
+% Sig.isTransparent = false;
 % Sig.beta = 1;  %Фактор спада
 % Sig.span = 16; %Количество символов
-% Sig.sps = 10; %Выборки на символ
+% Sig.sps = 100; %Выборки на символ
+% Sig.k =1;
 % Channel.time = 0; %Значение ошибок временной синхранизации
 % Channel.freq = 0; 
-% Mapper.Type = 'DQPSK';
+% Mapper.Type = 'QAM';
 % Mapper.ModulationOrder = 4;
-% Source.NumBitsPerFrame = 100; %по умолчанию 1000 
+% Source.NumBitsPerFrame = 1000; %по умолчанию 1000 
 % BER.BERNumRateDigits = 8;
 % BER.FERNumRateDigits = 5;
 % BER.BERPrecision = 5;
@@ -41,6 +45,7 @@ BER.h2dBMax = 10;
 % BER.h2dBInitStep = 1;
 % BER.h2dBMaxStep = 1;
 % BER.h2dBMinStep = 1;
-% % Отношения сигнал/шум:
-% BER.h2dBInit = 0;
+% 
+% 
+% BER.h2dBInit = 0;% Отношения сигнал/шум:
 % BER.h2dBMax = 10;
